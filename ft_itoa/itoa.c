0d0424f3a730a85724638749e113155d0deffb46
TheScrewdriver <kinoide56@gmail.com>
2022-02-15 19:26:52 +0100

    fix itoa and itoa_base

--- ft_itoa/itoa.c
+++ ft_itoa/itoa.c
@@ -6,17 +6,16 @@
 /*   By: romannbroque <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 17:16:37 by romannbroque      #+#    #+#             */
-/*   Updated: 2022/02/14 17:27:45 by romannbroque     ###   ########.fr       */
+/*   Updated: 2022/02/15 15:35:40 by romannbroque     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
-static size_t	get_sign(const int nb)
+static size_t	is_neg(const int nb)
 {
-	if (nb >= 0)
-		return (0);
-	return (1);
+	return (nb < 0);
 }
 
 static int	pow(const int nb, const int p)
@@ -30,45 +29,70 @@ static int	pow(const int nb, const int p)
 
 static size_t	get_n_size(const int nb)
 {
-	size_t	size;
-
-	size = 0;
 	if (nb == 0)
-		return (1);
-	while (nb / (pow(10, size)) != 0)
-		++size;
-	return (size);
+		return (0);
+	return (1 + get_n_size(nb / 10));
 }
 
-static void	attribute(char *nb_str, size_t size, size_t sign, int nb)
+/*
+static void	fill_str(char *nb_str, size_t rank, size_t max_rank, int nb)
+{
+	if (rank == max_rank)
+		nb_str[rank] = '\0';
+	else
+	{
+		int	digit;
+
+		digit = nb / 10;
+		nb -= digit * 10;
+		fill_str(nb_str, rank + 1, max_rank, nb / 10);
+		nb_str[rank] = '0' + digit;
+	}
+}
+*/
+
+static void	fill_str(char *nb_str, size_t size, int nb)
 {
 	size_t	i;
-	size_t	true_size;
 	int		digit;
 
 	i = 0;
-	if (nb < 0)
-		nb = -nb;
-	true_size = size + sign;
-	while (i < true_size)
+	while (i < size)
 	{
 		digit = nb / pow(10, size - 1 - i);
 		nb -= digit * pow (10, size - 1 - i);
-		nb_str[i + sign] = '0' + digit;
+		nb_str[i] = '0' + digit;
 		++i;
 	}
-	nb_str[true_size] = '\0';
+	nb_str[size] = '\0';
+}
+
+static int	get_abs(const int nb)
+{
+	if (nb < 0)
+		return (-nb);
+	return (nb);
 }
 
 char	*itoa(const int nb)
 {
 	const size_t	size = get_n_size(nb);
-	const size_t	sign = get_sign(nb);
+	const size_t	negativity = is_neg(nb);
 	char			*expr;
 
-	expr = (char *) malloc((size + sign + 1) * sizeof(char));
-	if (nb < 0)
-		expr[0] = '-';
-	attribute(expr, size, sign, nb);
+	expr = (char *) malloc((size + negativity + 1) * sizeof(char));
+	if (expr != NULL)
+	{
+		if (negativity)
+			expr[0] = '-';
+		fill_str(expr + negativity, size, get_abs(nb));
+	}
 	return (expr);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac > 1)
+		printf("%s\n", itoa(atoi(av[1])));
+	return (EXIT_SUCCESS);
 }
--- ft_itoa/itoa_base.c
+++ ft_itoa/itoa_base.c
@@ -6,24 +6,49 @@
 /*   By: romannbroque <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 18:13:51 by romannbroque      #+#    #+#             */
-/*   Updated: 2022/02/14 17:28:00 by romannbroque     ###   ########.fr       */
+/*   Updated: 2022/02/15 15:56:39 by romannbroque     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
+#define FORBIDDEN_CHAR "+-"
 
 static size_t	str_len(const char *str)
 {
 	size_t	len;
 
 	len = 0;
-	if (str != NULL)
-	{
-		while (str[len] != '\0')
-			++len;
-	}
+	while (str[len] != '\0')
+		++len;
 	return (len);
+}
+
+static size_t	is_neg(const int nb)
+{
+	return (nb < 0);
+}
+
+static	bool	is_forbidden_char(const char *base_charset)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (base_charset[i] != '\0')
+	{
+		j = 0;
+		while (FORBIDDEN_CHAR[j] != '\0')
+		{
+			if (base_charset[i] == FORBIDDEN_CHAR[j])
+				return (true);
+			++j;
+		}
+		++i;
+	}
+	return (false);
 }
 
 static int	pow(const int nb, const size_t p)
@@ -65,7 +90,8 @@ static size_t	get_size(const int nb, size_t len_base)
 	return (size);
 }
 
-static char	*get_coord(int nb, const char *base_charset, size_t size)
+static char	*get_coord(int nb, const char *base_charset,
+									size_t size, size_t sign)
 {
 	const size_t	len_base = str_len(base_charset);
 	size_t			i;
@@ -74,30 +100,48 @@ static char	*get_coord(int nb, const char *base_charset, size_t size)
 	char			*tr_expr;
 
 	i = size;
-	tr_expr = (char *) malloc((size + 1) * sizeof(char));
-	while (i > 0)
+	tr_expr = (char *) malloc((size + sign + 1) * sizeof(char));
+	if (tr_expr != NULL)
 	{
-		div = pow(len_base, i - 1);
-		unit = nb / div;
-		tr_expr[size - i] = base_charset[unit];
-		nb -= unit * div;
-		--i;
+		if (sign == 1)
+			tr_expr[0] = '-';
+		while (i > sign)
+		{
+			div = pow(len_base, i - 1);
+			unit = nb / div;
+			tr_expr[size - i] = base_charset[unit];
+			nb -= unit * div;
+			--i;
+		}
+		tr_expr[size + sign] = '\0';
 	}
-	tr_expr[size] = '\0';
 	return (tr_expr);
 }
 
 static char	*itoa_base(const int nb, const char *base_charset)
 {
-	const size_t	len = str_len(base_charset);
-	size_t			size;
-	char			*expr;
-
-	if ((is_double(base_charset) == false) && (len > 1))
+	if (base_charset != NULL)
 	{
-		size = get_size(nb, len);
-		expr = get_coord(nb, base_charset, size);
-		return (expr);
+		const size_t	len = str_len(base_charset);
+		const size_t	negativity = is_neg(nb);
+		size_t			size;
+		char			*expr;
+
+		if ((is_forbidden_char(base_charset) == false)
+			&& (is_double(base_charset) == false)
+			&& (len > 1))
+		{
+			size = get_size(nb, len);
+			expr = get_coord(nb, base_charset, size, negativity);
+			return (expr);
+		}
 	}
 	return (NULL);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac > 2)
+		printf("%s\n", itoa_base(atoi(av[1]), av[2]));
+	return (EXIT_SUCCESS);
 }
